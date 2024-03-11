651e28fffb8c19f8fca1e4e5d5e29ff858048126
mousou85 <mousou85@gmail.com>
2024-03-11 14:46:07 +0900

    feat: 상품 관리 모듈 작업

--- lib/item/item_screen.dart
+++ lib/item/item_screen.dart
@@ -14,16 +14,31 @@ class ItemScreen extends StatefulWidget {
 class _ItemScreenState extends State<ItemScreen> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: itemSampleData
-          .map(
-            (item) => ListItem(
-              itemName: item['name'],
-              itemType: item['type'],
-              createdAt: item['createdAt'],
-            ),
-          )
-          .toList(),
+    return Scaffold(
+      body: ListView(
+        children: itemSampleData
+            .map(
+              (item) => ListItem(
+                itemName: item['name'],
+                itemType: item['type'],
+                createdAt: item['createdAt'],
+              ),
+            )
+            .toList(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 50),
+        child: IconButton(
+          icon: const Icon(Icons.add, color: Colors.white),
+          iconSize: 40,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+            padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
